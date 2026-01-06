defa3b632bf68b9989856f452a5b439e5d87cf72
théodore clément <213218350+thclemnt@users.noreply.github.com>
2026-01-06 12:52:08 +0100

    build: bump version

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_DATAINJECTION_VERSION', '3.0.1');
+define ('PLUGIN_DATAINJECTION_VERSION', '3.0.2');
 
 if (!defined("PLUGIN_DATAINJECTION_UPLOAD_DIR")) {
     define("PLUGIN_DATAINJECTION_UPLOAD_DIR", GLPI_PLUGIN_DOC_DIR."/datainjection/");
